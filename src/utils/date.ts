959439498ff28f978e2d77389968fd661bd7586c
mevagos-del <mevagos@gmail.com>
2026-06-04 12:28:51 +0300

    Delete src directory

--- src/utils/date.ts
+++
@@ -1,6 +0,0 @@
-export function formatDateTime(value: Date = new Date()) {
-  return new Intl.DateTimeFormat('uk-UA', {
-    dateStyle: 'medium',
-    timeStyle: 'short',
-  }).format(value);
-}
--- src/utils/registerServiceWorker.ts
+++
@@ -1,9 +0,0 @@
-export function registerServiceWorker() {
-  if ('serviceWorker' in navigator && import.meta.env.PROD) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/service-worker.js').catch(() => {
-        // PWA registration failure should not block the business workflow.
-      });
-    });
-  }
-}
